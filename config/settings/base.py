b38258b79286d1d29e9b9032ae93867937a38b65
LucasZimm <lucas.zimmermann@bam.de>
2026-01-05 11:58:30 +0100

    edit timezone

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "MEZ"
+TIME_ZONE = "Europe/Berlin"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#languages
@@ -325,7 +325,9 @@ ACCOUNT_FORMS = {"signup": "openbis_upload_helper.users.forms.UserSignupForm"}
 # https://docs.allauth.org/en/latest/socialaccount/configuration.html
 SOCIALACCOUNT_ADAPTER = "openbis_upload_helper.users.adapters.SocialAccountAdapter"
 # https://docs.allauth.org/en/latest/socialaccount/configuration.html
-SOCIALACCOUNT_FORMS = {"signup": "openbis_upload_helper.users.forms.UserSocialSignupForm"}
+SOCIALACCOUNT_FORMS = {
+    "signup": "openbis_upload_helper.users.forms.UserSocialSignupForm"
+}
 
 
 # Your stuff...
