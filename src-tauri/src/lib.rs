a637ffc0cc784155ddb89108f33f79fe6098bac5
jpizarro <jose.pizarro-blanco@bam.de>
2026-09-16 12:15:56 +0200

    Added the pyinstaller binary a proper Tauri sidecar artifact

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1114,6 +1114,7 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             login,
             get_spaces,
